b1198e0b5f0274d8dc09c56efc731ceeb499ad40
Dzulfaqor Ali Dipangegara <whysoserius30@gmail.com>
2024-05-17 19:37:03 +0700

    update giant dan jester

--- app/src/main/java/tubes/oop/pvz/JesterZombie.java
+++ app/src/main/java/tubes/oop/pvz/JesterZombie.java
@@ -3,11 +3,11 @@ package tubes.oop.pvz;
 public class JesterZombie extends Zombie {
     
     public JesterZombie(int x, int y) {
-        super("Jester Zombie", 200, 100, 1, false, x, y, 0);
+        super("Jester Zombie", 200, 100, 1  , false, x, y, 0);
     }
 
     @Override
     public boolean isSlow() {
-        return false; // Jester Zombie is immune to slowing effects
+        return false;  // Jester Zombie kebal terhadap efek slow
     }
 }
