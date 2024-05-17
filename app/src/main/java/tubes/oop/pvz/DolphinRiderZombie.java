7a8276054b1661e745755b68ae79a426065a2a6d
Dzulfaqor Ali Dipangegara <whysoserius30@gmail.com>
2024-05-17 19:16:02 +0700

    implemen interface di kelas PoleVault dan Dolphinrider

--- app/src/main/java/tubes/oop/pvz/DolphinRiderZombie.java
+++ app/src/main/java/tubes/oop/pvz/DolphinRiderZombie.java
@@ -1,9 +1,28 @@
 package tubes.oop.pvz;
 
-public class DolphinRiderZombie extends Zombie {
-    private boolean hasJumped = false;
+public class DolphinRiderZombie extends Zombie implements SpecialMove {
+
+    private boolean hasJumped;
 
     public DolphinRiderZombie(int x, int y) {
         super("Dolphin Rider Zombie", 175, 100, 1, true, x, y, 0);
+        this.hasJumped = false;
+    }
+
+    @Override
+    public void specialMove(Tile currentTile, Tile nextTile) {
+        try {
+            if (!hasJumped && nextTile.getPlant() != null) {
+                nextTile.removePlant();
+                this.hasJumped = true;
+                // Logika untuk melompat ke tile berikutnya
+                this.setX(this.getX() - 2); // Melompat dua tile ke depan
+            } else {
+                // Gerakan normal jika sudah melompat atau tidak ada tanaman di tile berikutnya
+                this.setX(this.getX() - 1);
+            }
+        } catch (InvalidPositionException e) {
+            System.err.println("Invalid position: " + e.getMessage());
+        }
     }
 }
--- app/src/main/java/tubes/oop/pvz/PoleVaultingZombie.java
+++ app/src/main/java/tubes/oop/pvz/PoleVaultingZombie.java
@@ -1,10 +1,28 @@
 package tubes.oop.pvz;
 
-public class PoleVaultingZombie extends Zombie {
+public class PoleVaultingZombie extends Zombie implements SpecialMove {
+
+    private boolean hasJumped;
 
     public PoleVaultingZombie(int x, int y) {
         super("Pole Vaulting Zombie", 175, 100, 1, false, x, y, 0);
+        this.hasJumped = false;
     }
 
-    //ini harus ada atribut tambahan
+    @Override
+    public void specialMove(Tile currentTile, Tile nextTile) {
+        try {
+            if (!hasJumped && nextTile.getPlant() != null) {
+                nextTile.removePlant();
+                this.hasJumped = true;
+                // Logika untuk melompat ke tile berikutnya
+                this.setX(this.getX() - 2); // Melompat dua tile ke depan
+            } else {
+                // Gerakan normal jika sudah melompat atau tidak ada tanaman di tile berikutnya
+                this.setX(this.getX() - 1);
+            }
+        } catch (InvalidPositionException e) {
+            System.err.println("Invalid position: " + e.getMessage());
+        }
+    }
 }
